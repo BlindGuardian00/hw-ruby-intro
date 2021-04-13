7b9e0369a9d11137368fad4e4c603a06fd42098b
Daniel Mitchell <dmitche3@uccs.edu>
2021-04-13 06:20:40 +0000

    Part 3 completed and comments were added throughout

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,4 +1,8 @@
 # When done, submit this entire file to the autograder.
+#Programmer: Daniel Mitchell
+#Class: CS3300
+#Due: 4-18-2021
+#Assignment: Iteration 2, extra credit
 
 # Part 1
 
@@ -23,6 +27,9 @@ def max_2_sum arr
    return largest + second_largest
 end
 
+#Returns a bool telling whether the array arr contains two
+#distinct values that sum to n (that is, values in distinct
+#array positions, even if two values repeat in the array)
 def sum_to_n? arr, n
   two_sum_to_n = false
   for i in 0...arr.length
@@ -39,10 +46,17 @@ def hello(name)
   return "Hello, #{name}"
 end
 
+#returns a bool telling whether the string
+#s starts with a consonant
 def starts_with_consonant? s
   return (s =~ /^[^aeiouAEIOU\W].*/)
 end
 
+#returns a bool telling whether the
+#string passed in was (1) a valid
+#binary number and (2) a multiple of
+#4 (which occurs precisely when the
+#two least significant digits are both 0)
 def binary_multiple_of_4? s
   return s =~ /^[0-1]*0?0$/
 end
@@ -50,5 +64,24 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError unless (isbn.class == String && isbn != "")
+    @isbn = isbn
+    raise ArgumentError unless (price > 0)
+    @price = price
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+
+  #returns a string with the format $\d+.\d\d
+  #where \d refers to any digit, and the above is
+  #written as a regular expression; i.e., it is
+  #any number followed by a decimal point and two
+  #digits.
+  def price_as_string
+    "$#{"%0.2f" % price}"
+  end
+
+
 end
